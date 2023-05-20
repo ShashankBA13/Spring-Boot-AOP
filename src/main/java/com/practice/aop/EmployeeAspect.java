3ed8ff0d3617568abe06d568bb4a7a773e38d6e3
Dante Alligheri <shashank996419@gmail.com>
2023-05-20 14:39:22 +0530

    Including the aop for service layer.

--- src/main/java/com/practice/aop/EmployeeAspect.java
+++ src/main/java/com/practice/aop/EmployeeAspect.java
@@ -16,15 +16,32 @@ public class EmployeeAspect {
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSSSS");
 
+    //For Controller Layer
     @Before(value = "execution(* com.practice.controller.EmployeeController.*(..))")
     public void beforeAdvice(JoinPoint joinPoint) {
         LocalDateTime now = LocalDateTime.now();
-        System.out.println("Request to " + joinPoint.getSignature() + " started at " +now.format(formatter));
+        System.out.println("Request to " + joinPoint.getSignature() + " started at " + now.format(formatter));
     }
 
+    // For Controller Layer
     @After(value = "execution(* com.practice.controller.EmployeeController.*(..))")
     public void afterAdvice(JoinPoint joinPoint) {
         LocalDateTime now = LocalDateTime.now();
         System.out.println("Request to " + joinPoint.getSignature() + " ended at " + now.format(formatter));
     }
+
+    //For Service layer
+    @Before(value = "execution(* com.practice.service.EmployeeService.*(..))")
+    public void beforeAdviceForService(JoinPoint joinPoint) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Request to " + joinPoint.getSignature() + " started at " + now.format(formatter));
+    }
+
+    // For Service Layer
+    @After(value = "execution(* com.practice.service.EmployeeService.*(..))")
+    public void afterAdviceforService(JoinPoint joinPoint) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Request to " + joinPoint.getSignature() + " ended at " + now.format(formatter));
+    }
+
 }
